d443a3a388952186d7daf06a7440f3af9a5db59c
mku777 <mkm.kuz777@gmail.com>
2022-10-09 15:34:38 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,29 @@
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+const galleryElements = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item =>
+        `
+          <a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+      `
+    )
+    .join('');
+}
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+galleryElements.insertAdjacentHTML('beforeend', addGalleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
