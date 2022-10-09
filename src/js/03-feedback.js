78a891f62da804b5b866b1b753a414d8a0ef2bbf
mku777 <mkm.kuz777@gmail.com>
2022-10-09 19:14:26 +0300

    add const STORAGE_KEY for 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,24 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-
 const formData = {};
 
 function onFormData(event) {
   formData[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function onSubmitForm(event) {
-  const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+function onFormSubmit(event) {
+  const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
   event.preventDefault();
   console.log(parsedData);
   event.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function dataFromLocalStorage() {
-  const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
   if (parsedData) {
@@ -29,4 +29,4 @@ function dataFromLocalStorage() {
 
 dataFromLocalStorage();
 form.addEventListener('input', throttle(onFormData, 500));
-form.addEventListener('submit', onSubmitForm);
+form.addEventListener('submit', onFormSubmit);
