6a1b951f2243f74149130da0b3cc75f740cd0979
mku777 <mkm.kuz777@gmail.com>
2022-10-09 18:50:17 +0300

    fix task 2 videoplayer-current-time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 }
 player.on('timeupdate', throttle(onPlay, 1000));
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
