0c100e91bc4162d7db5029f696b6305b0bd500a8
mku777 <mkm.kuz777@gmail.com>
2022-10-09 16:45:48 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
 import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+
+const formDataObj = {};
+
+function onFormDataObj(event) {
+    event.preventDefault();
+    formDataObj[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formDataObj));
+}
+
+
+function onGetData (event) {
+   const savedData = localStorage.getItem("feedback-form-state");
+   const parsedData = JSON.parse(savedData);
+}
